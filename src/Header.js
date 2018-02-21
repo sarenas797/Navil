9c092bc5480f1cce885f7289c2308167795a96ea
Samuel Arenas <sarenas@purdue.edu>
2018-02-21 14:20:04 -0600

    Make fixed Navbar

--- src/Header.js
+++ src/Header.js
@@ -5,9 +5,18 @@ import './Header.css'
 class Header extends Component{
     render(){
         return(
-            <div className ="Header"> 
-            <h1>Navil</h1>
-            <img src={logo} height="500" width="650"/>
+            <div className ="Header">
+                <div className = "navbar">
+                    <ul>
+                        <li>< a href=''>ullamcorper</a></li>
+                        <li>< a href=''>eleifend</a></li>
+                        <li>< a href=''>elementum</a></li>
+                        <li>< a href=''>Aliquam</a></li>
+                        <li>< a href=''>Praesent</a></li>
+                    </ul>
+                </div>
+                <h1>Navil</h1>
+                <img src={logo} height="475" width="650"/>
             </div>
         )
     }
